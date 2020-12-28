c5293beb87e5757065cfcf43c5ab3a2814e7d13b
georgeCherry02 <georgeb.cherry@gmail.com>
2020-12-28 15:50:58 +0000

    Corrected pattern for observation id search

--- fetch_training_set_data/determine_obs_ids.py
+++ fetch_training_set_data/determine_obs_ids.py
@@ -8,6 +8,7 @@ from astroquery.mast import Observations
 parser = argparse.ArgumentParser()
 parser.add_argument("in_file", help="Path to csv file containing TCEs for sector")
 parser.add_argument("out_file", help = "File to write tic id list output to")
+parser.add_argument("sector", help="Sector number observations are from")
 args = parser.parse_args()
 
 ### Read in the tce data
@@ -23,13 +24,19 @@ for tic_id in tic_ids:
         continue
     tic_ids_already_checked.append(tic_id)
 
+    only_tic_id = str(tic_id)
     tic_id = "TIC "+str(tic_id)
     print(f"Looking for two min observations of {tic_id}")
-    two_min_observations = Observations.query_criteria(objectname=tic_id, dataproduct_type="timeseries", obs_collection="TESS", obs_id="*-0120-s")
+    obs_name_pattern = "*-s*"+str(args.sector)+"-0000*"+only_tic_id+"*-s"
+    two_min_observations = Observations.query_criteria(objectname=tic_id, dataproduct_type="timeseries", obs_collection="TESS", obs_id=obs_name_pattern)
     if (len(two_min_observations) > 0):
-        print(f"Found observations fitting description for {tic_id}")
+        print(f"Found observations matching {tic_id}")
+        ### For some reason this returns multiple tic id observations?
+        ### Therefore filter by target_name again
+        two_min_observations = two_min_observations[two_min_observations["target_name"] == only_tic_id]
         for obs_id in two_min_observations["obsid"]:
             obs_ids_with_two_min_cadences.append(obs_id)
+        tic_ids_with_two_min_cadences.append(tic_id)
 
 output = "obs_id\n"
 for obs_id in obs_ids_with_two_min_cadences:
